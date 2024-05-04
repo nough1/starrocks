64a4dca809718b5e4e43556cead8f5ffeea37e43
simba.hu <simba.hu@nio.com>
2024-05-04 08:30:34 +0800

    fix check style issue

--- fe/fe-core/src/main/java/com/starrocks/qe/Coordinator.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/Coordinator.java
@@ -939,7 +939,7 @@ public class Coordinator {
                             if (result.status.errorMsgs != null && !result.status.errorMsgs.isEmpty()) {
                                 errMsg = result.status.errorMsgs.get(0);
                             }
-                            LOG.info("call be params:{},result:{}",GsonUtils.GSON.toJson(pair.first),GsonUtils.GSON.toJson(result));
+                            LOG.info("call be param:{},res:{}", GsonUtils.GSON.toJson(pair.first), GsonUtils.GSON.toJson(result));
                         } catch (ExecutionException e) {
                             LOG.warn("catch a execute exception", e);
                             code = TStatusCode.THRIFT_RPC_ERROR;
@@ -1249,7 +1249,7 @@ public class Coordinator {
                             PExecBatchPlanFragmentsResult result =
                                     pair.second.get(queryDeliveryTimeoutMs, TimeUnit.MILLISECONDS);
                             code = TStatusCode.findByValue(result.status.statusCode);
-                            LOG.info("batch call be params:{},result:{}",GsonUtils.GSON.toJson(pair.first),GsonUtils.GSON.toJson(result));
+                            LOG.info("b call be params:{},res:{}", GsonUtils.GSON.toJson(pair.first), GsonUtils.GSON.toJson(result));
                             if (result.status.errorMsgs != null && !result.status.errorMsgs.isEmpty()) {
                                 errMsg = result.status.errorMsgs.get(0);
                             }
